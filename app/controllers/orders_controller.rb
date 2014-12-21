447dbc3ccf7eb79594a4004d0e95ae4d6b30421d
Eduardo Gutiérrez Silva <gt.silva.e@gmail.com>
2014-12-21 02:45:21 -0600

    Transfer support
    Transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -49,6 +49,13 @@ class OrdersController < ApplicationController
 
     end
 
+    # Transfer to user
+    transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
+
     # Saving and redirecting
     @order.save
 
